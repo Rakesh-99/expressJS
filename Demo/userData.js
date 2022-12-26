5cc7bbfb0649cc9dea0204dc9764a318a55f1eb9
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-12-27 01:23:36 +0530

    rendered a simple api

---
+++ Demo/userData.js
@@ -0,0 +1,16 @@
+const userData = [
+    {
+        name: 'Rakesh Kumar Parida',
+        age: 23,
+        occupation: 'Student',
+        domain: 'IT'
+    },
+    {
+        name: 'Bikash Kumar Parida',
+        age: 23,
+        occupation: 'Student',
+        domain: 'Mech'
+    }
+];
+
+module.exports = userData;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 
 const express = require('express');
+const userData = require('./Demo/userData');
 const app = express();
 
 
@@ -17,6 +18,9 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.sendFile(__dirname +'/Demo/about.html');
 });
+app.get('/userData', (req, res) => {
+    res.send(JSON.stringify(userData));
+})
 
 
 app.listen(2020);
@@ -25,9 +29,10 @@ app.listen(2020);
 // nodeJS static routing
 
 
-
+/*
 const http = require('http');
 const fs = require('fs');
+const userData = require('./Demo/userData');
 
 let server = http.createServer((req, res) => {
     const getUrl = req.url;
@@ -35,18 +40,26 @@ let server = http.createServer((req, res) => {
 
     if (getUrl == '/') {
         const readFile = fs.readFileSync('./Demo/index.html');
+        res.writeHead(200, {'content-type': 'text/html'})
         res.write(readFile);
         res.end();
 
     } else if (getUrl == '/about') {
         const readFile = fs.readFileSync('./Demo/about.html');
+        res.writeHead(200, { 'content-type': 'text/html' })
         res.write(readFile);
         res.end();
 
     } else if (getUrl == '/contact') {
         const readFile = fs.readFileSync('./Demo/contact.html');
+        res.writeHead(200, { 'content-type': 'text/html' })
         res.write(readFile);
         res.end();
-    };
+    } else if (getUrl == '/userData') {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.write(JSON.stringify(userData));
+        res.end();
+    }
 });
 server.listen(2020);
+*/
